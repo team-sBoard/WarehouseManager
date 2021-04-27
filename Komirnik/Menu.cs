58f281b9290566cb8e8855d4cfc36a7f785344b0
Dozimetrist <@rr.rr>
2021-04-27 13:57:45 +0300

    Added 2 Orders Menu and Product management as well. Product management one is commented since there's a nedd to learn how to grab something from the list. Also updated Komirnik menu

--- Komirnik/Menu.cs
+++ Komirnik/Menu.cs
@@ -20,16 +20,27 @@
         {
             return "[Komirnik - Menu] - Komirnik - Order management \n" +
                " 1.1) Issue shipping order \n" +
-               " 1.2) Issue recieving order  \n" +
+               " 1.2) Issue receiving order  \n" +
                " 1.3) Back ";
         }
-        public static string KomirnikProductManagement()
+        public static string KomirnikProductManagementMenu()
         {
             return "[Komirnik - Menu] - Komirnik - Add product \n" +
                " 2.1) Create product \n" +
                " 2.2) Update product  \n" +
                " 2.3) Delete product \n " +
                " 2.4) Back ";
+        }
+
+        public static string KomirnikProductDisplayMenu()
+        {
+            return "[Komirnik - Menu - Komirnik - ]3.1) Display products by Name \n" +
+                "3.2) Display products by Origin \n" +
+                "3.3) Display products by Category \n" +
+                "3.4) Display all the countries-manufacturers \n" +
+                "3.5) Find products for specific Country \n" +
+                "3.6) Display everything \n" +
+                "3.7) Back";
         }
     }
 }
--- Komirnik/MenuService.cs
+++ Komirnik/MenuService.cs
@@ -37,21 +37,27 @@ namespace Komirnik
             do
             {
                 Console.WriteLine(Menu.KomirnikMenu());
-                Console.WriteLine("Please, type menu №");
+                Console.WriteLine("Please, enter the menu №");
                 command = Console.ReadLine();
 
                 switch (command)
                 {
                     case "1":
-                        Menu.KomirnikOrderManagementMenu();
+                        KomirnikOrderManagement();
                         break;
                     case "2":
-                        Menu.KomirnikProductManagement();
+                        KomirnikProductManagement();
                         break;
                     case "3":
-                        MenuService.DisplayProductList();
+                        //KomirnikProductDisplay();
                         break;
-                    case "4":
+                    case "4":                       
+                        break;
+                    case "5":
+                        break;
+                    case "6":
+                        break;
+                    case "7":
                         break;
                     default:
                         Console.WriteLine("Invalid selected " + command + " item of menu. Please, select the proper part");
@@ -61,21 +67,211 @@ namespace Komirnik
             Console.WriteLine("Cya later!");
         }
 
-        public static void DisplayProductList()
-        {
-                Console.WriteLine("Students:");
-                Console.WriteLine("Id: FirstName LastName");
+            public static void KomirnikOrderManagement()
+            {
+                string command;
 
-                foreach (Product product in )
+                do
                 {
-                Console.WriteLine(string.Format(
-                        "{1} {2} {3} {4}",
-                        product.name,
-                        product.origin,
-                        product.quantity,
-                        product.type));
-                }
+                    Console.WriteLine(Menu.KomirnikOrderManagementMenu());
+                    Console.WriteLine("Please, enter the menu №");
+                    command = Console.ReadLine();
+
+                    switch (command)
+                    {
+                        case "1":
+                            CreateShippingOrder();
+                            break;
+                        case "2":
+                            CreateRecievingOrder();
+                            break;
+                        case "3":
+                            break;
+
+                        default:
+                            Console.WriteLine("Invalid selected " + command + " item of menu. Please, select the proper part");
+                            break;
+                    }
+                } while (command != "3");
+                Console.WriteLine("Returning back");
+            }
+
+            private static void CreateRecievingOrder()
+            {
+                Console.WriteLine("You have issued a Receiving order");
+            }
+
+            private static void CreateShippingOrder()
+        {
+            Console.WriteLine("You have issued a Shipping Order");
         }
+        public void KomirnikProductManagement()
+        {
+            string command;
+
+            do
+            {
+                Console.WriteLine(Menu.KomirnikProductManagementMenu());
+                Console.WriteLine("Please, enter the menu №");
+                command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case "1":
+                        CreateProduct();
+                        break;
+                    case "2":
+                        UpdateProduct();
+                        break;
+                    case "3":
+                        DeleteProduct();
+                        break;
+                    case "4":
+                        break;                       
+                    default:
+                        Console.WriteLine("Invalid selected " + command + " item of menu. Please, select the proper part");
+                        break;
+                }
+            } while (command != "4");
+            Console.WriteLine("Returning back");
+        }
+
+        private static void DeleteProduct()
+        {
+            Console.WriteLine("You have deleted banana");
+        }
+
+        private static void UpdateProduct()
+        {
+            Console.WriteLine("You have updated banana");
+        }
+
+        private static void CreateProduct()
+        {
+            Console.WriteLine("You have created banana");
+        }                  
+
+        //private static void KomirnikProductDisplay()
+        //{
+        //    string command;
+        //    do
+        //    {
+        //        Console.WriteLine(Menu.KomirnikProductDisplayMenu());
+        //        Console.WriteLine("Please, enter the menu №");
+        //        command = Console.ReadLine();
+
+        //        switch (command)
+        //        {
+        //            case "1":
+        //                DisplayByName();
+        //                break;
+        //            case "2":
+        //                DisplayByOrigin();
+        //                break;
+        //            case "3":
+        //                DisplayByCategory();
+        //                break;
+        //            case "4":
+        //                DisplayAllCountries();
+        //                break;
+        //            case "5":
+        //                DisplayProductForCountry();
+        //                break;
+        //            case "6":
+        //                DisplayEverything();
+        //                break;
+        //            case "7":
+        //                break;
+        //            default:
+        //                Console.WriteLine("Invalid selected " + command + " item of menu. Please, select the proper part");
+        //                break;
+        //        }
+        //    } while (command != "7");
+        //    Console.WriteLine("Returning back");
+        //}
+
+
+        //public static void DisplayByName()
+        //{
+        //    Console.WriteLine("Here the full list of goods sorted by their Name :");
+        //    Console.WriteLine("Product Name: Country of Origin, Quantity");
+
+        //    foreach (Product product in )
+        //    {
+        //        Console.WriteLine(string.Format(
+        //                "{1} {2} {3}",
+        //                product.name,
+        //                product.origin,
+        //                product.quantity));
+        //    }
+        //}
+        //public static void DisplayByOrigin()
+        //{
+        //    Console.WriteLine("Here the full list of goods sorted by Country of Origin :");
+        //    Console.WriteLine("Product Name: Country of Origin, Quantity");
+
+        //    foreach (Product product in )
+        //    {
+        //        Console.WriteLine(string.Format(
+        //                "{1} {2} {3} {4}",
+        //                product.name,
+        //                product.origin,
+        //                product.quantity));
+        //    }
+        //}
+        //public static void DisplayByCategory()
+        //{
+        //    Console.WriteLine("Here the full list of goods sorted by Category :");
+        //    Console.WriteLine("Category, Product Name, Quantity");
+
+        //    foreach (Product product in )
+        //    {
+        //        Console.WriteLine(string.Format(
+        //                "{1} {2} {3} {4}",
+        //                product.name,
+        //                product.type,
+        //                product.quantity));
+        //    }
+        //}
+        //public static void DisplayAllCountries()
+        //{
+        //    Console.WriteLine("Here the full list of Countries-manufacturers:");
+        //    Console.WriteLine("Country name: ");
+
+        //    foreach (Product product in )
+        //    {
+        //        Console.WriteLine(string.Format(
+        //                "{1}",
+        //                product.origin));
+        //    }
+        //}
+        //public static void DisplayProductForCountry()
+        //{
+        //    Console.WriteLine("Here the the product list for:" + "Тут нужно вставлять ту страну, которую введёт пользователь"); //Подумать над методом решения
+
+        //    foreach (Product product in )
+        //    {
+        //        Console.WriteLine(string.Format(
+        //                "{1} {2}",
+        //                product.origin,
+        //                product.type));
+        //    }
+        //}
+        //public static void DisplayEverything()
+        //{
+        //    Console.WriteLine("Here the full list of goods:");
+        //    Console.WriteLine("Product Name: Country of Origin, Quantity, Category");
+
+        //    foreach (Product product in )
+        //    {
+        //        Console.WriteLine(string.Format(
+        //                "{1} {2} {3} {4}",
+        //                product.name,
+        //                product.origin,
+        //                product.quantity,
+        //                product.type));
+        //    }
+        //}
 
         public void AddKomirnik()
         {
